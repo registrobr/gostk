4ed6b857180b78e915b4bc988eec672e62be45ad
Rafael Dantas Justo <rafael@registro.br>
2016-06-08 16:10:40 -0300

    Refactor and fix harcoded values in db connection
    The values MaxIdleConnections and MaxOpenConnections weren't using the input
    variables. Also moved the private newTx function to an anonymous function that
    made the code more readable.

--- db/db.go
+++ db/db.go
@@ -1,29 +1,53 @@
-// Package db handles new connections and new transactions with database
+// Package db handles new connections and new transactions with database adding an extra layer to
+// deal with timeouts and other connection problems during the operation.
 package db
 
 import (
-	"time"
+	"database/sql"
 	"errors"
 	"fmt"
-	"database/sql"
+	"time"
 )
 
+// ErrNewTxTimedOut returned when takes too long to retrieve a transaction from the database server.
 var ErrNewTxTimedOut = errors.New("new transaction timed out")
 
-// Data is all the data needed to create a new connection with
-type Data struct {
-	Username string
-	Password string
-	DatabaseName string
-	Host string
+// PostgresDriver used to connect using a postgres driver. You should import it somewhere in your
+// code. By default "postgres" is used from github.com/lib/pq. We don't import directly here to
+// don't stick this library with only one type of Postgres driver, and to make it more flexible for
+// testing.
+//
+//     import (
+//       _ "github.com/lib/pq"
+//     )
+var PostgresDriver = "postgres"
 
-	ConnectTimeout time.Duration
+// Data is all the data needed to create a new connection
+type Data struct {
+	Username     string
+	Password     string
+	DatabaseName string
+	Host         string
+
+	ConnectTimeout   time.Duration
 	StatementTimeout time.Duration
 
 	MaxIdleConnections int
 	MaxOpenConnections int
 }
 
+// NewData returns the database connection parameters with some default values.
+func NewData() Data {
+	return Data{
+		Host:               "127.0.0.1",
+		ConnectTimeout:     3 * time.Second,
+		StatementTimeout:   10 * time.Second,
+		MaxIdleConnections: 16,
+		MaxOpenConnections: 32,
+	}
+}
+
+// ConnectPostgres used to connect to any Postgres database
 func ConnectPostgres(d Data) (db *sql.DB, err error) {
 	connParams := fmt.Sprintf(
 		"user=%s password=%s dbname=%s sslmode=disable host=%s connect_timeout=%d statement_timeout=%d",
@@ -35,37 +59,37 @@ func ConnectPostgres(d Data) (db *sql.DB, err error) {
 		int(d.StatementTimeout.Seconds()),
 	)
 
-	if db, err = sql.Open("postgres", connParams); err != nil {
+	if db, err = sql.Open(PostgresDriver, connParams); err != nil {
 		return nil, err
 	}
 
-	db.SetMaxIdleConns(16)
-	db.SetMaxOpenConns(32)
-
-	return db, err
+	db.SetMaxIdleConns(d.MaxIdleConnections)
+	db.SetMaxOpenConns(d.MaxOpenConnections)
+	return
 }
 
-func newTx(db *sql.DB, ch chan *sql.Tx, chErr chan error) {
-	tx, err := db.Begin()
-	if err != nil {
-		chErr <- err
-		return
-	}
-
-	ch <- tx
-}
-
-func NewTx(db *sql.DB, timeout time.Duration) (tx *sql.Tx, err error)  {
+// NewTx starts a new database transaction adding an extra layer to deal with database timeouts
+// during the action.
+func NewTx(db *sql.DB, timeout time.Duration) (*sql.Tx, error) {
 	ch := make(chan *sql.Tx, 1)
 	chErr := make(chan error, 1)
-	go newTx(db, ch, chErr)
+
+	go func() {
+		tx, err := db.Begin()
+		if err != nil {
+			chErr <- err
+			return
+		}
+
+		ch <- tx
+	}()
 
 	select {
-	case tx = <-ch:
+	case tx := <-ch:
 		return tx, nil
+	case err := <-chErr:
+		return nil, err
 	case <-time.After(timeout):
 		return nil, ErrNewTxTimedOut
 	}
-
-	return nil, ErrNewTxTimedOut
 }
