db1aebb63814e380bc7c8d0e80a58ff901e12a38
Rafael Dantas Justo <rafael@registro.br>
2016-06-21 07:49:55 -0300

    Remove unused function
    The current Equal function isn't really used in projects.

--- errors/compare.go
+++ errors/compare.go
@@ -2,27 +2,6 @@
 // and log levels.
 package errors
 
-// Equal compares the low level errors values.
-func Equal(first, second error) bool {
-	err1, ok1 := first.(traceableError)
-	err2, ok2 := second.(traceableError)
-
-	if ok1 {
-		if ok2 {
-			return err1.err == err2.err
-		}
-
-		return err1.err == second
-
-	}
-
-	if ok2 {
-		return err2.err == first
-	}
-
-	return first == second
-}
-
 // EqualMsg compares the errors messages.
 func EqualMsg(first, second error) bool {
 	if first == nil || second == nil {
